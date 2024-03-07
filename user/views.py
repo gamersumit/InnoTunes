c68fc1358030b1fd6c7afd2fd18f37b167286a96
dev <saggarwal@innow8apps.com>
2024-03-07 12:15:52 +0530

    implemetned password restet, otp, and mail functionalites also made an api to provide details for current user request by frontend

--- user/views.py
+++ user/views.py
@@ -1,7 +1,10 @@
+from logging import raiseExceptions
+from tkinter import EXCEPTION
 from django.utils import timezone
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.utils import timezone
 from music.serializers import SongSerializer
 from .serializers import *
 from .models import User
@@ -9,9 +12,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework import permissions
 from django.contrib.auth import authenticate
 from user.models import User
-from utils.utils import CommonUtils
+from utils.utils import CommonUtils, Mail
 from comment.models import SongLikes, AlbumLikes, PlaylistLikes
-
+from django.core.mail import send_mail
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView) :
@@ -119,9 +122,21 @@ class UserListView(generics.ListAPIView) :
         return queryset
     
         
-
+class CurrentUserDetailView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]  
+    
+    def get(self, request):
+        try:
+            user = UserUtils.getUserFromToken(request.headers['Authorization'].split(" ")[1])
+            serializer = self.serializer_class(user)
+            return Response(serializer.data, status = 200)
         
-# ArtistSerializer --- to provide list of all artist
+        except Exception as e:
+            return Response({'message' : str(e)}, status = 400)
+        
+        
 class ArtistListView(generics.ListAPIView) :
     serializer_class = ArtistSerializer
     queryset = User.objects.filter(is_artist = True)
@@ -132,16 +147,101 @@ class ArtistDetailView(generics.RetrieveAPIView):
     queryset = User.objects.filter(is_artist = True)
     permission_classes = [permissions.IsAuthenticated]
    
+
+
+class SendPasswordResetOTPView(generics.UpdateAPIView):
+    serializer_class = MailOTPSerializer
+    queryset = MailOTP.objects.all()
     
+    def put(self, request):
+        try:
+            subject = 'Passwrod Reset Verfication Mail'
+            body = CommonUtils.otp_generator()
+            email = request.data['email']
+            mail = Mail(subject,f'OTP: {str(body)}', [email])
+            
+            if User.objects.filter(email = email).exists():
+                user = User.objects.get(email = email)
+                
+                if MailOTP.objects.filter(user_id = user.id).exists():
+                    serializer = MailOTP.objects.get(user_id = user.id)
+                    serializer.otp = body
+                    
+                else :
+                    data = {'otp' : body, 'user_id' : user.id}
+                    serializer = self.serializer_class(data = data)
+                    serializer.is_valid(raise_exception=True)
+                
+                mail.send()
+                serializer.save()
+            
+                return Response({'message' : 'mail sent succesfully'}, status=200)
+            
+            else :
+                raise Exception('EMAIL NOT FOUND')
+            
+        except Exception as e:
+            print(str(e))
+            return Response({'message' : str(e)}, status = 400)
     
+
+class resetPasswordTokenGenerationView(APIView):
+    http_method_names = ['post']
+    def post(self, request):
+        try :
+            otp = request.data['otp']
+            email = request.data['email']
+            if User.objects.filter(email = email).exists():
+                user = User.objects.get(email = email)
+            else : raise Exception('email not found')
+            
+            if MailOTP.objects.filter(user_id = user.id).exists():
+                obj = MailOTP.objects.get(user_id = user.id)
+            else : raise Exception('try resending otp')
+            
+            five_minutes_ago = timezone.now() - timezone.timedelta(minutes=5)
+            if obj.updated_at < five_minutes_ago:
+                raise Exception('otp expired')
+            
+            if obj.otp != int(otp):
+                raise Exception('incorrect otp')
+            
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token' : str(token)}, status = 200)
+        
+        except Exception as e:
+            return Response({'message': str(e)}, status = 400)
+        
+class resetPasswordView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def patch(self, request):
+        try :
+            print(request.headers['Authorization'].split(" ")[1])
+            user = UserUtils.getUserFromToken(request.headers['Authorization'].split(" ")[1])
+            print(user)
+            data = {'password' : request.data['password']}
+            serializer = self.serializer_class(user, data = data, partial = True)
+            print(user)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        
+            return Response({'message' : 'password reset successfully'}, status = 200)
+        except Exception as e:
+            return Response({'message': str(e)}, status = 400)
+        
 # SHORT NAMING :
 user_register_view = RegisterView.as_view()
 user_logout_view = LogoutView.as_view()
 user_detail_view = UserDetailView.as_view()
+current_user_detail_view = CurrentUserDetailView.as_view()
 user_list_view = UserListView.as_view()
 user_login_view = LoginView.as_view()
 artist_list_view = ArtistListView.as_view()
 artist_detail_view = ArtistDetailView.as_view()
-
+send_otp_password_reset_view = SendPasswordResetOTPView.as_view()
+reset_password_token_generation_view = resetPasswordTokenGenerationView.as_view()
+reset_password = resetPasswordView.as_view()
 
 
