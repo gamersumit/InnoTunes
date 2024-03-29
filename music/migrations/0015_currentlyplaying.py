01cb92229dd81e249e85fd85616af28ffd83db4c
Vanshika Batra <vbatra@innow8apps.com>
2024-03-29 16:12:13 +0530

    updated migrations

--- music/migrations/0015_currentlyplaying.py
+++ music/migrations/0015_currentlyplaying.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('music', '0015_playlist_is_global'),
+        ('music', '0014_alter_song_genre'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
