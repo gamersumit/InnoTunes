0fc0a8ad2cf82ea2efb1626bd403056d15158810
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-04-06 19:33:58 +0530

    requirements and asgi wsgi

--- innotune/asgi.py
+++ innotune/asgi.py
@@ -11,8 +11,11 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innotune.settings')
+load_dotenv()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS'))
 # Combine the HTTP and WebSocket applications
 import user.routing
 application = ProtocolTypeRouter({
