cf2025d3ddafef4e00b9577d9f46a5521a84575e
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-04-06 21:43:13 +0530

    base url

--- innotune/urls.py
+++ innotune/urls.py
@@ -48,6 +48,6 @@ urlpatterns = [
     path('rest/', include('rest_framework.urls', namespace='rest_framework')),
    
     # Swagger
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
