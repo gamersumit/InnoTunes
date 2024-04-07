71b18c4cc9a5c6a59c2dc715329f877a81297846
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-04-08 01:01:40 +0530

    urlpatterns

--- innotune/urls.py
+++ innotune/urls.py
@@ -21,6 +21,9 @@ from rest_framework import permissions, authentication
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from user.views import PatchLogoutView
 
@@ -51,3 +54,5 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
