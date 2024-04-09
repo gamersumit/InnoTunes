a0adfdc15cd44b8e9b0b0a3c39bfb27322092c24
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-04-09 14:26:12 +0530

    https

--- innotune/settings/render.py
+++ innotune/settings/render.py
@@ -6,17 +6,7 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 
 # Static files (CSS, JavaScript, Images)
@@ -24,6 +14,6 @@ MIDDLEWARE = [
 
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
